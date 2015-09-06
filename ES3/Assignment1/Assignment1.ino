d79873cef2bec70476284a86769fd647b8503baf
Diep,Minh-Triet M <m.diep@student.fontys.nl>
2015-09-06 20:49:58 +0200

    Finish converting Assignment 1 to port manips

--- ES3/Assignment1/Assignment1.ino
+++ ES3/Assignment1/Assignment1.ino
@@ -1,50 +1,67 @@
 /*
-   Pins:  10    Port:  B2
-          12           B4
-          5            D5
-          6            D6
+   Pins:  10    Port:  B2  Button
+          12           B4  Button
+          5            D5  LED
+          6            D6  LED
+          3            D3  Buzzer
           
+   Buttons: 10 & 12
+   LEDs   : 5  & 6
+   Buzzer : 3
 */
+
+//Buzzer help thing
+bool toot = true;
 
 void setup() {
   Serial.begin(9600);
   
-  DDRB |= _BV(2) | _BV(4);
+  // Simultaneously set DDD5 and DDD6 as output
+  // Also DDD3
+  DDRD |= _BV(DDD5) | _BV(DDD6) | _BV(DDD3);
   
-  //pinMode(5, OUTPUT);
-  //pinMode(6, OUTPUT);
+  // Set DDB2 and DDB4 as input
+  DDRB  &= ~(_BV(DDB2) | _BV(DDB4));
   
-  DDRD &= !(_BV(5) | _BV(6));
-  PORTD |= _BV(5) | _BV(6);
-  //pinMode(10, INPUT_PULLUP);
-  //pinMode(12, INPUT_PULLUP);
-  
-  
+  // Pullup bits
+  //PORTB |=   _BV(PORTB2) | _BV(PORTB4);  
 }
 
 void loop() {
-  int d10 = digitalRead(10);
-  int d12 = digitalRead(12);
+  // Read and store pins d10 & d12
+  int d10 = PINB & _BV(PINB2);
+  int d12 = PINB & _BV(PINB4);
 
-  if (!d10) {
-    digitalWrite(5, HIGH);
+  if (d10) {
+    // Enable bit PORTD5
+    PORTD |= _BV(PORTD5);
   }
   else
   {
-    digitalWrite(5, LOW);
+    // Disable bit PORTD5
+    PORTD &= ~_BV(PORTD5);
   }
   
-  if (!d12) {
-    digitalWrite(6, HIGH);
+  if (d12) {
+    PORTD |= _BV(PORTD6);
     Serial.print("Hello World!");
-    delay(1000);
   }
   else
   {
-    digitalWrite(6, LOW);
+    PORTD &= ~_BV(PORTD6);
   }
-  if (!d10 && !d12) {
-    // tone();
+  if (d10 && d12) {
+    if (toot) {
+      PORTD &= ~_BV(PORTD3);
+    }
+    else {
+      PORTD |= _BV(PORTD3);
+    }
+    toot = !toot;
+    //Serial.print stealing precious microseconds
+    //Also I don't know how to make non-blocking sounds
+    //in combination with port manipulation manually
+    //delayMicroseconds(500);
   }
 }
 
