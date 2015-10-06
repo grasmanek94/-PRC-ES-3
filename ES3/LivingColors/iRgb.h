cdde9ab97a31b52ad29e045b49672b51ef435958
Gamer_Z <grasmanek94@gmail.com>
2015-10-06 14:17:41 +0200

    fix mh errorz

--- ES3/LivingColors/iRgb.h
+++ ES3/LivingColors/iRgb.h
@@ -1,21 +1,22 @@
 #ifndef IRGB_H
 #define IRGB_H
 
+enum ERGBOutput
+{
+	Led,
+	Serialp
+};
+
 class iRgb
 {
 private:
 public:
-	enum EOutput
-	{
-		Led,
-		Serialp
-	};
+
 	//virtual void Init() = 0;
 	virtual void SetColor(int hue) = 0;
 
 };
 
-typedef iRgb::EOutput ERGBOutput;
 #endif
 
 
--- ES3/LivingColors/iSensor.h
+++ ES3/LivingColors/iSensor.h
@@ -1,19 +1,20 @@
 #ifndef ISENSOR_H
 #define ISENSOR_H
 
+enum SensorType
+{
+	Ultrasonic,
+	Infrared,
+	Serialp
+};
+
 class iSensor
 {
 private:
 public:
-	enum ESensor
-	{
-		Ultrasonic,
-		Infrared,
-		Serialp
-	};
+
 	//virtual void Init() = 0;
 	virtual int GetValue() = 0;
 };
 
-typedef iSensor::ESensor SensorType;
 #endif
