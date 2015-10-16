b31acf530275b0450756a40b8e264cf8cc21bb1b
Gamer_Z <grasmanek94@gmail.com>
2015-10-17 01:33:06 +0200

    done

--- ES3/IPC_CriticalSections/proga.c
+++ ES3/IPC_CriticalSections/proga.c
@@ -7,14 +7,21 @@
 #include <sys/stat.h>
 #include <sys/fcntl.h>
 
+typedef struct
+{
+	int prog_a_count;
+	int prog_b_count;
+} nice_print;
+
 static int shm_fd = -1;
-char* shm_addr;
+nice_print* shm_addr;
 int rtnval = 0;
-int size = 26;
-const char* alphabet = "abcdefghijklmnopqrstuvwzyz";
 const char* shm_name = "ipc4";
+int size = sizeof(nice_print);
 
 int main(void) {
+	stdout = freopen("/dev/tty1", "w", stdout);
+
     shm_fd = shm_open(shm_name, O_CREAT | O_EXCL | O_RDWR, 0600);
     if (shm_fd == -1) {
         perror ("ERROR: shm_open() failed");
@@ -23,20 +30,22 @@ int main(void) {
 	if (rtnval != 0) {
         perror ("ERROR: ftruncate() failed");
     }
-    shm_addr = (char *) mmap (NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    shm_addr = (nice_print *) mmap (NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if (shm_addr == MAP_FAILED) {
         perror ("ERROR: mmap() failed");
     }
+	memset(shm_addr, 0, size);
 	
-	for (int i = 0; i < size; i++) {
-		shm_addr[i] = alphabet[i];
-	}
-	
-	while (shm_addr[0] != 'A') {
-	}
-	
-	printf ("data (@ %p): '%s'\n", shm_addr, shm_addr);
-	
+	printf("1\n");
+	++shm_addr->prog_a_count;
+	while (shm_addr->prog_b_count < 1) {}
+	printf("3\n");
+	++shm_addr->prog_a_count;
+	while (shm_addr->prog_b_count < 2) {}
+	printf("5\n");
+	++shm_addr->prog_a_count;
+	while (shm_addr->prog_b_count < 3) {}
+
 	rtnval = close (shm_fd);
 	if (rtnval != 0) {
 		perror ("ERROR: close() failed");
--- ES3/IPC_CriticalSections/progb.c
+++ ES3/IPC_CriticalSections/progb.c
@@ -8,38 +8,41 @@
 #include <sys/stat.h>
 #include <sys/fcntl.h>
 
+typedef struct
+{
+	int prog_a_count;
+	int prog_b_count;
+} nice_print;
+
 static int  shm_fd = -1;
-char* shm_addr;
 int rtnval = 0;
-int size;
-char* alphabet;
 const char* shm_name = "ipc4";
+nice_print* shm_addr;
+int size = sizeof(nice_print);
 
 int main(void)
 {
+	stdout = freopen("/dev/tty1", "w", stdout);
+
     shm_fd = shm_open (shm_name, O_RDWR, 0600);
     if (shm_fd == -1) {
         perror ("ERROR: shm_open() failed");
     }
-    
-    size = lseek (shm_fd, 0, SEEK_END);
-    shm_addr = (char *) mmap (NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    shm_addr = (nice_print *) mmap (NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if (shm_addr == MAP_FAILED) {
         perror ("ERROR: mmap() failed");
     }
     
-	while (shm_addr[0] != 'a')	//wacht totdat byte 0 in shm 'a' is
-	{ }
+	while (shm_addr->prog_a_count < 1) {}
+	printf("2\n");
+	++shm_addr->prog_b_count;
+	while (shm_addr->prog_a_count < 2) {}
+	printf("4\n");
+	++shm_addr->prog_b_count;
+	while (shm_addr->prog_a_count < 3) {}
+	printf("6\n");
+	++shm_addr->prog_b_count;
 
-	alphabet = (char*)malloc(size+1);
-    for (int i = 0; i < size; i++) {
-		alphabet[i] = shm_addr[i] + 'A' - 'a';
-	}
-	alphabet[size] = 0;
-
-	printf("%s\n", alphabet);
-	free(alphabet);
-
-	shm_addr[0] += 'A' - 'a';	//maak van kleine letter 'a' hoofdletter 'A'
 	return (0);					//stop
 }
