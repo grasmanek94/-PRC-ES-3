16a9fce16be475ef10f89dcb740e7ed89f092634
Gamer_Z <grasmanek94@gmail.com>
2015-11-02 10:25:34 +0100

    prep assignment 8

---
+++ PRC3/Assignment8/app/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+int main( void )
+{
+    return 0;
+}
---
+++ PRC3/Assignment8/libraries/googlemock/CMakeFiles/gmock_main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/hgfs/VMShared/gmock-1.7.0/gtest/src/gtest-all.cc" "/mnt/hgfs/VMShared/gmock-1.7.0/CMakeFiles/gmock_main.dir/gtest/src/gtest-all.cc.o"
+  "/mnt/hgfs/VMShared/gmock-1.7.0/src/gmock-all.cc" "/mnt/hgfs/VMShared/gmock-1.7.0/CMakeFiles/gmock_main.dir/src/gmock-all.cc.o"
+  "/mnt/hgfs/VMShared/gmock-1.7.0/src/gmock_main.cc" "/mnt/hgfs/VMShared/gmock-1.7.0/CMakeFiles/gmock_main.dir/src/gmock_main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "."
+  "gtest/include"
+  "gtest"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
