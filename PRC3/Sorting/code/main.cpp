94893039d486824bfb6178cc434af36da40cc4f0
Gamer_Z <grasmanek94@gmail.com>
2015-12-07 14:22:33 +0100

    testing

--- PRC3/Sorting/code/main.cpp
+++ PRC3/Sorting/code/main.cpp
@@ -4,39 +4,6 @@
 
 #include "MergeSort.h"
 
-#include <iostream>
-#include <time.h>
-class TM
-{
-private:
-	clock_t start;
-	clock_t end;
-	double cpu_time_used;
-public:
-	TM()
-		: start(0), end(0), cpu_time_used(0.0)
-	{ }
-
-	void Start()
-	{
-		start = clock();
-	}
-
-	void Stop()
-	{
-		end = clock();
-		cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-	}
-
-	double Elapsed()
-	{
-		return cpu_time_used;
-	}
-};
-
-TM timemeasure;
-
-
 int main()
 {
 	FileStructure f;
@@ -44,8 +11,22 @@ int main()
 	Key* ckey = NULL;
 	Key* new_head = NULL;
 
-	f.loadFile("data/gibberish.bin", *head);
-	timemeasure.Start();
+	head->addValue("ac_ik");
+	head->addValue("ac_ben");
+	head->addValue("ac_meneer");
+	head->addValue("ac_rafal");
+	head->addValue("ac_meneer");
+	head->addValue("ac_ben");
+	head->addValue("ac_ik");
+
+	head->addValue("ab_ik");
+	head->addValue("ab_ben");
+	head->addValue("ab_meneer");
+	head->addValue("ab_rafal");
+	head->addValue("ab_meneer");
+	head->addValue("ab_ben");
+	head->addValue("ab_ik");
+	//f.loadFile("data/gibberish.bin", *head);
 
 	ckey = head;
 	while (ckey)
@@ -55,9 +36,7 @@ int main()
 	}
 	new_head = MergeSort::Sort(head);
 
-	timemeasure.Stop();
-
-	std::cout << timemeasure.Elapsed() * 1000.0 << " ms" << std::endl;
+	new_head->print();
 
 	f.saveFile(*new_head, "sorted.bin");
 
