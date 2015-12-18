d3bc80eb309f5233446162f4be46462b6b24521e
Gamer_Z <grasmanek94@gmail.com>
2015-12-18 01:37:42 +0100

    be nice

--- EL/el-applicatie/cgi.cxx
+++ EL/el-applicatie/cgi.cxx
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <map>
+
+#include "shared_data.hxx"
 
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems)
 {
@@ -20,8 +23,29 @@ std::vector<std::string> split(const std::string &s, char delim)
 	return elems;
 }										
 
+std::map<std::string, std::vector<std::string>> ParseUrlGetData(const std::vector<std::string>& gets)
+{
+	std::map<std::string, std::vector<std::string>> k_v;
+
+	for (auto& i : gets)
+	{
+		std::string key;
+		std::string value;
+		size_t positionOfEquals = i.find("=");
+		key = i.substr(0, positionOfEquals);
+		if (positionOfEquals != std::string::npos)
+		{
+			value = i.substr(positionOfEquals + 1);
+		}
+		k_v[key].push_back(value);
+	}
+	return k_v;
+}
+
 int main()
-{																			
+{			
+	int shm_fd;
+	InputReport* report = cgi_shm_create(shm_fd);
 	std::cout																
 		<< "Content-type: text/html" << std::endl							
 		<< "Connection: close" << std::endl									
@@ -40,9 +64,11 @@ int main()
 		
 	if (request_args.size() < 1)
 	{
-		std::cout << "ERROR: HOW THE FUCK IS THIS EVEN POSSIBLE? Cosmic rays bombarding the processor and memory?" << std::endl;
+		std::cout << "ERROR: HOW THE HELL IS THIS EVEN POSSIBLE? Cosmic rays bombarding the processor and memory?" << std::endl;
 		return 0;
 	}
+
+	std::map<std::string, std::vector<std::string>> k_v = ParseUrlGetData(request_args);
 
 
 	return 0;
